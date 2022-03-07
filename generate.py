182c24466371ff025f12fe77cb1ce7ab75ea0ac7
Hallee Wong <hew1@williams.edu>
2022-03-07 14:37:06 -0500

    restrict gpu

--- generate.py
+++ generate.py
@@ -32,6 +32,18 @@ def num_range(s: str) -> List[int]:
     vals = s.split(',')
     return [int(x) for x in vals]
 
+def restrict_GPU_pytorch(gpuid, use_cpu=False):
+    """
+        gpuid: str, comma separated list "0" or "0,1" or even "0,1,3"
+    """
+    if not use_cpu:
+        os.environ["CUDA_VISIBLE_DEVICES"] = gpuid
+
+        print("Using GPU:{}".format(gpuid))
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        print("Using CPU")
+
 #----------------------------------------------------------------------------
 
 @click.command()
@@ -43,6 +55,7 @@ def num_range(s: str) -> List[int]:
 @click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
 @click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
 @click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+@click.option('--gpu_id', help='Which GPU to use', type=str, default='0')
 def generate_images(
     ctx: click.Context,
     network_pkl: str,
@@ -50,6 +63,7 @@ def generate_images(
     truncation_psi: float,
     noise_mode: str,
     outdir: str,
+    gpu_id: str,
     class_idx: Optional[int],
     projected_w: Optional[str]
 ):
@@ -77,6 +91,8 @@ def generate_images(
     python generate.py --outdir=out --projected_w=projected_w.npz \\
         --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
     """
+
+    restrict_GPU_pytorch(gpu_id, use_cpu=False)
 
     print('Loading networks from "%s"...' % network_pkl)
     device = torch.device('cuda')
@@ -118,7 +134,7 @@ def generate_images(
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}_trunc{truncation_psi}.png')
 
 
 #----------------------------------------------------------------------------
